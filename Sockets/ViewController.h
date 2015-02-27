a122cd603d44ed159f57079775c0111206a569c4
Ilya Tsarev <idreyk@gmail.com>
2015-02-27 14:43:56 +0300

    Initial Commit

---
+++ Sockets/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sockets
+//
+//  Created by Ilya Tsarev on 27.02.15.
+//  Copyright (c) 2015 Ilya Tsarev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
